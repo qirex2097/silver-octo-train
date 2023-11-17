13699957b2f83111d2985750cf286a37407a0342
Masaru Hori <qirex_rr@yahoo.co.jp>
2023-11-17 23:35:06 +0900

    termion 追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello");
+use std::io::{stdin, stdout, Write};
+use termion::{screen::IntoAlternateScreen, raw::IntoRawMode};
+use termion::event::{Event, Key};
+use termion::input::TermRead;
+
+fn main() -> Result<(), std::io::Error> {
+    let stdout = stdout().into_alternate_screen()?;
+    let mut stdout = stdout.into_raw_mode()?;
+
+    write!(stdout, "{}", "Hello")?;
+    stdout.flush()?;
+
+    let stdin = stdin();
+    for event in stdin.events() {
+        if event? == Event::Key(Key::Ctrl('c')) {
+            return Ok(());
+        }
+    }
+
+    Ok(())
 }
