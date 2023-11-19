3b89ba5189c1ad9c062168bb7dc82ad7d4a69337
Masaru Hori <qirex_rr@yahoo.co.jp>
2023-11-19 09:18:33 +0900

    add lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub struct DispField {
     pub disp_arr: [[char; 19]; 19],
+    pub cursor: (usize, usize),
 }
 
 impl DispField {
@@ -26,16 +27,103 @@ impl DispField {
             [ '+', '-', '+', '-', '+', '-', '+', '-', '+', '-', '+', '-', '+', '-', '+', '-', '+', '-', '+', ],
         ];
 
-        DispField { disp_arr, }
+        DispField { disp_arr, cursor: (1, 1), }
     }
 }
+
+pub fn get_cell_coords(cursor: (usize, usize)) -> (usize, usize) {
+    (cursor.0.saturating_sub(1) / 2, cursor.1.saturating_sub(1) / 2)
+}
+fn get_cursor_from_cell_coords(cell_coords: (usize, usize)) -> (usize, usize) {
+    (cell_coords.0 * 2 + 1, cell_coords.1 * 2 + 1)
+}
+pub fn cursor_left(cursor: (usize, usize)) -> (usize, usize) {
+    (std::cmp::max(cursor.0 - 1, 1), cursor.1)
+}
+pub fn cursor_right(cursor: (usize, usize)) -> (usize, usize) {
+    (std::cmp::min(cursor.0 + 1, 18 - 1), cursor.1)
+}
+pub fn cursor_up(cursor: (usize, usize)) -> (usize, usize) {
+    (cursor.0, std::cmp::max(cursor.1 - 1, 1))
+}
+pub fn cursor_down(cursor: (usize, usize)) -> (usize, usize) {
+    (cursor.0, std::cmp::min(cursor.1 + 1, 18 - 1))
+
+}
+
+pub fn cursor_left_cell(cursor: (usize, usize)) -> (usize, usize) {
+    let (cell_x, cell_y) = get_cell_coords(cursor);
+    let cell_x = if cursor.0 % 2 == 1 { cell_x } else { cell_x + 1 };
+    let (cursor_x, _) = get_cursor_from_cell_coords((cell_x.saturating_sub(1), cell_y));
+    (cursor_x, cursor.1)
+}
+pub fn cursor_right_cell(cursor: (usize, usize)) -> (usize, usize) {
+    let (cell_x, cell_y) = get_cell_coords(cursor);
+    let (cursor_x, _) = get_cursor_from_cell_coords((std::cmp::min(cell_x + 1, 8), cell_y));
+    (cursor_x, cursor.1)
+}
+pub fn cursor_up_cell(cursor: (usize, usize)) -> (usize, usize) {
+    let (cell_x, cell_y) = get_cell_coords(cursor);
+    let cell_y = if cursor.1 % 2 == 1 { cell_y } else { cell_y + 1 };
+    let (_, cursor_y) = get_cursor_from_cell_coords((cell_x, cell_y.saturating_sub(1)));
+    (cursor.0, cursor_y)
+}
+pub fn cursor_down_cell(cursor: (usize, usize)) -> (usize, usize) {
+    let (cell_x, cell_y) = get_cell_coords(cursor);
+    let (_, cursor_y) = get_cursor_from_cell_coords((cell_x, std::cmp::min(cell_y + 1, 8)));
+    (cursor.0, cursor_y)
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
+    fn cursor_move() {
+        assert_eq!(cursor_left_cell((3, 3)), (1, 3));
+        assert_eq!(cursor_left_cell((2, 3)), (1, 3));
+        assert_eq!(cursor_left_cell((1, 3)), (1, 3));
+        assert_eq!(cursor_left_cell((4, 4)), (3, 4));
+        assert_eq!(cursor_right_cell((1, 1)), (3, 1));
+        assert_eq!(cursor_right_cell((2, 1)), (3, 1));
+        assert_eq!(cursor_right_cell((16, 1)), (17, 1));
+        assert_eq!(cursor_right_cell((17, 1)), (17, 1));
+        assert_eq!(cursor_up_cell((1, 1)), (1, 1));
+        assert_eq!(cursor_up_cell((1, 2)), (1, 1));
+        assert_eq!(cursor_up_cell((1, 3)), (1, 1));
+        assert_eq!(cursor_down_cell((1, 17)), (1, 17));
+        assert_eq!(cursor_down_cell((1, 16)), (1, 17));
+        assert_eq!(cursor_down_cell((1, 15)), (1, 17));
+        assert_eq!(cursor_down_cell((2, 14)), (2, 15));
+    }
+    #[test]
+    fn test_get_cell_coords() {
+        assert_eq!(get_cell_coords((1, 1)), (0, 0));
+        assert_eq!(get_cursor_from_cell_coords((0, 0)), (1, 1));
+        assert_eq!(get_cell_coords((2, 1)), (0, 0));
+        assert_eq!(get_cell_coords((3, 1)), (1, 0));
+        assert_eq!(get_cursor_from_cell_coords((1, 0)), (3, 1));
+        assert_eq!(get_cell_coords((3, 3)), (1, 1));
+        assert_eq!(get_cursor_from_cell_coords((1, 1)), (3, 3));
+        assert_eq!(get_cell_coords((7, 7)), (3, 3));
+    }
+    #[test]
     fn it_works() {
-        let disp: DispField = DispField::new();
+        let mut disp: DispField = DispField::new();
+        assert_eq!(get_cell_coords(disp.cursor), (0, 0));
+        assert_eq!(disp.cursor, (1, 1));
+        disp.cursor = cursor_left(disp.cursor);
+        assert_eq!(disp.cursor, (1, 1));
+        disp.cursor = cursor_right(disp.cursor);
+        assert_eq!(disp.cursor, (2, 1));
+        disp.cursor = cursor_up(disp.cursor);
+        assert_eq!(disp.cursor, (2, 1));
+        disp.cursor = cursor_down(disp.cursor);
+        assert_eq!(disp.cursor, (2, 2));
+        disp.cursor = cursor_left(disp.cursor);
+        assert_eq!(disp.cursor, (1, 2));
+        disp.cursor = cursor_up(disp.cursor);
+        assert_eq!(disp.cursor, (1, 1));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,67 +4,67 @@ use termion::{screen::IntoAlternateScreen, raw::IntoRawMode, raw::RawTerminal};
 use termion::event::{Event, Key};
 use termion::input::TermRead;
 
-use silver_octo_train::DispField;
+use silver_octo_train::*;
 
 fn main() -> Result<(), std::io::Error> {
+    let mut disp: DispField = DispField::new();
+
     let stdout = stdout().into_alternate_screen()?;
     let mut stdout = stdout.into_raw_mode()?;
 
-    let mut disp: DispField = DispField::new();
-    let disp_arr: &mut [[char; 19]; 19] = &mut disp.disp_arr;
-
-    let mut cursor_x: usize = 1;
-    let mut cursor_y: usize = 1;
-
     write!(stdout, "{}{}", clear::All, cursor::BlinkingBlock)?;
-    draw(&mut stdout, &disp_arr)?;
-    write!(stdout, "{}", cursor::Goto(cursor_x as u16 + 1, cursor_y as u16 + 1))?;
+    draw(&mut stdout, &mut disp.disp_arr)?;
+    write!(stdout, "{}", cursor::Goto(disp.cursor.0 as u16 + 1, disp.cursor.1 as u16 + 1))?;
     stdout.flush()?;
 
     let stdin = stdin();
     for event in stdin.events() {
-        let mut cell_x = cursor_x.saturating_sub(1) / 2;
-        let mut cell_y = cursor_y.saturating_sub(1) / 2;
-        let prev_cursor_x = cursor_x;
-        let prev_cursor_y = cursor_y;
-        write!(stdout, "{}", clear::All)?;
+        let (prev_cursor_x, prev_cursor_y) = disp.cursor;
         let event = event?;
         match event {
             Event::Key(Key::Ctrl('c')) | Event::Key(Key::Char('q')) => {
                 break;
             }
             Event::Key(Key::Char('h')) => {
-                cursor_x = std::cmp::max(cursor_x - 1, 1);
+                disp.cursor = cursor_left(disp.cursor);
             }
             Event::Key(Key::Char('l')) => {
-                cursor_x = std::cmp::min(cursor_x + 1, 18 - 1);
+                disp.cursor = cursor_right(disp.cursor);
             }
             Event::Key(Key::Char('k')) => {
-                cursor_y = std::cmp::max(cursor_y - 1, 1);
+                disp.cursor = cursor_up(disp.cursor);
             }
             Event::Key(Key::Char('j')) => {
-                cursor_y = std::cmp::min(cursor_y + 1, 18 - 1);
+                disp.cursor = cursor_down(disp.cursor);
             }
             Event::Key(Key::Char('H')) => {
-                cell_x = if cursor_x % 2 == 1 { cell_x } else { cell_x + 1 };
-                cursor_x = std::cmp::max(cell_x.saturating_sub(1) * 2 + 1, 1);
+                disp.cursor = cursor_left_cell(disp.cursor);
+                let (cursor_x, cursor_y) = disp.cursor;
+                let disp_arr = &mut disp.disp_arr;
                 disp_arr[cursor_y][cursor_x + 1] = if cursor_y % 2 == 1 && prev_cursor_x != cursor_x { ' ' } else { disp_arr[cursor_y][cursor_x + 1] };
             }
             Event::Key(Key::Char('L')) => {
-                cursor_x = std::cmp::min(cell_x.saturating_add(1) * 2 + 1, 18 - 1);
+                disp.cursor = cursor_right_cell(disp.cursor);
+                let (cursor_x, cursor_y) = disp.cursor;
+                let disp_arr = &mut disp.disp_arr;
                 disp_arr[cursor_y][cursor_x - 1] = if cursor_y % 2 == 1 && prev_cursor_x != cursor_x { ' ' } else { disp_arr[cursor_y][cursor_x - 1] };
             }
             Event::Key(Key::Char('K')) => {
-                cell_y = if cursor_y % 2 == 1 { cell_y } else { cell_y + 1 };
-                cursor_y = std::cmp::max(cell_y.saturating_sub(1) * 2 + 1, 1);
+                disp.cursor = cursor_up_cell(disp.cursor);
+                let (cursor_x, cursor_y) = disp.cursor;
+                let disp_arr = &mut disp.disp_arr;
                 disp_arr[cursor_y + 1][cursor_x] = if cursor_x % 2 == 1 && prev_cursor_y != cursor_y { ' ' } else { disp_arr[cursor_y + 1][cursor_x] };
             }
             Event::Key(Key::Char('J')) => {
-                cursor_y = std::cmp::min(cell_y.saturating_add(1) * 2 + 1, 18 - 1);
+                disp.cursor = cursor_down_cell(disp.cursor);
+                let (cursor_x, cursor_y) = disp.cursor;
+                let disp_arr = &mut disp.disp_arr;
                 disp_arr[cursor_y - 1][cursor_x] = if cursor_x % 2 == 1 && prev_cursor_y != cursor_y { ' ' } else { disp_arr[cursor_y - 1][cursor_x] };
             }
             Event::Key(Key::Char(' ')) => {
-                match (cursor_x % 2, cursor_y %2) {
+                let (cursor_x, cursor_y) = disp.cursor;
+                let disp_arr = &mut disp.disp_arr;
+                match (cursor_x % 2, cursor_y % 2) {
                     (1, 0) => { disp_arr[cursor_y][cursor_x] = if disp_arr[cursor_y][cursor_x] == ' '  { '-' } else { ' ' } }
                     (0, 1) => { disp_arr[cursor_y][cursor_x] = if disp_arr[cursor_y][cursor_x] == ' '  { '|' } else { ' ' } }
                     _ => {}
@@ -72,9 +72,10 @@ fn main() -> Result<(), std::io::Error> {
             }
             _ => {}
         }
-        draw(&mut stdout, &disp_arr)?;
-        write!(stdout, "{}{},{}", cursor::Goto(1, 20), cursor_x, cursor_y)?;
-        write!(stdout, "{}", cursor::Goto(cursor_x as u16 + 1, cursor_y as u16 + 1))?;
+
+        write!(stdout, "{}", clear::All)?;
+        draw(&mut stdout, &mut disp.disp_arr)?;
+        write!(stdout, "{}", cursor::Goto(disp.cursor.0 as u16 + 1, disp.cursor.1 as u16 + 1))?;
         stdout.flush()?;
     }
 
