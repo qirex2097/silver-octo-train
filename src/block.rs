e1efc3755d1771604b9fa10bd137f66c1d87db97
Masaru Hori <qirex_rr@yahoo.co.jp>
2023-12-16 09:07:08 +0900

    search_combination_for_cum 実装

--- src/block.rs
+++ src/block.rs
@@ -1,4 +1,6 @@
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Block {
@@ -37,9 +39,106 @@ impl Block {
     }
 }
 
+impl Block {
+    fn _search_combination_for_product(&self) -> Vec<u8> {
+        vec![0]
+    }
+    fn _search_combination_for_division(&self) -> Vec<u8> {
+        vec![0]
+    }
+    fn _search_combination_for_difference(&self) -> Vec<u8> {
+        vec![0]
+    }
+}
+
+//----------------------------------------
+impl Block {
+    pub fn search_combination_for_sum(&self) -> Vec<Vec<u8>> {
+        let combinations = {
+            let mut stack: Vec<(Vec<u8>, usize)> = Vec::new();
+            let mut result: Vec<Vec<u8>> = Vec::new();
+
+            stack.push((Vec::new(), 0));
+            while let Some((combination, sum)) = stack.pop() {
+                if sum == self.value && combination.len() == self.cells.len() {
+                    result.push(combination);
+                } else if combination.len() < self.cells.len() {
+                    let max_digit = if let Some(&max) = combination.iter().min() {
+                        max
+                    } else {
+                        9
+                    };
+                    for digit in 1..=max_digit as usize {
+                        let mut combination = combination.clone();
+                        combination.push(digit as u8);
+                        stack.push((combination, sum + digit));
+                    }
+                }
+            }
+
+            result
+        };
+
+        self.find_candidate_combinations(&combinations)
+    }
+    fn find_candidate_combinations(&self, combinations: &Vec<Vec<u8>>) -> Vec<Vec<u8>> {
+        let mut result: Vec<Vec<u8>> = Vec::new();
+        for combination in combinations {
+            'outer: for perm in combination.iter().permutations(combination.len()) {
+                let perm: Vec<u8> = perm.into_iter().map(|x| *x).collect();
+                let mut tate: Vec<HashSet<u8>> = vec![HashSet::new(); 10];
+                let mut yoko: Vec<HashSet<u8>> = vec![HashSet::new(); 10];
+                for (i, &cell) in self.cells.iter().enumerate() {
+                    let (x, y) = (cell % 10, cell / 10);
+                    let value = perm[i];
+                    if tate[x].contains(&value) || yoko[y].contains(&value) {
+                        continue 'outer;
+                    }
+                    tate[x].insert(value);
+                    yoko[y].insert(value);
+                }
+                if !result.contains(&perm) {
+                    result.push(perm);
+                }
+            }
+        }
+        result
+    }
+}
+
 #[cfg(test)]
 mod test_block {
     use super::*;
+    #[test]
+    fn test_search_combination_for_sum() {
+        let block = Block {
+            cells: vec![11, 12, 22],
+            value: 20,
+        };
+        let combi = block.search_combination_for_sum();
+        assert_eq!(combi.len(), 28);
+
+        let block = Block {
+            cells: vec![11, 12],
+            value: 1,
+        };
+        let combi = block.search_combination_for_sum();
+        assert_eq!(combi.len(), 0);
+
+        let block = Block {
+            cells: vec![67, 68, 69, 78],
+            value: 11,
+        };
+        let combi = block.search_combination_for_sum();
+        assert_eq!(combi.len(), 52);
+
+        let block = Block {
+            cells: vec![11, 12, 22],
+            value: 21,
+        };
+        let combi = block.search_combination_for_sum();
+        assert_eq!(combi.len(), 21);
+    }
     #[test]
     fn test_get_width_and_get_height() {
         let block = Block {
